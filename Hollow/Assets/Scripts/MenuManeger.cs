6b2383b44f05c4fc05092167062951c96815ab23
Iago-V-Schmitz <iago.schmitz007@gmail.com>
2025-06-10 10:40:15 -0300

    Menu Feito

--- Hollow/Assets/Scripts/MenuManeger.cs
+++ Hollow/Assets/Scripts/MenuManeger.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Concurrent;
+using UnityEngine.SceneManagement;
 
-public class MenuManeger
+public class MenuManeger : MonoBehaviour
 {
-    
+    [SerializeField] private string nomeDoleveldojogo;
+    [SerializeField] private GameObject painelmenuinicial;
+    [SerializeField] private GameObject painelopcoes;
+    public void jogar()
+    {
+        SceneManager.LoadScene(nomeDoleveldojogo);
+    }
+    public void abriropcoes()
+    {
+        painelmenuinicial.SetActive(false);
+        painelopcoes.SetActive(true);
+    }
+    public void fecharopcoes()
+    {
+        painelopcoes.SetActive(false);
+        painelmenuinicial.SetActive(true);
+       
+    }
+    public void sairjogo()
+    {
+        Debug.Log("Sair do jogo");
+        Application.Quit();
+    }
 }
