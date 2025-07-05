ea45d5be95152f9dd083a497dfe0976d2099374c
LucasPagnoncelli <lucaspagnoncelli9@gmail.com>
2025-07-04 22:14:36 -0300

    movimento do jogador arrumado

--- Hollow/Assets/Scripts/Movimentojogador.cs
+++ Hollow/Assets/Scripts/Movimentojogador.cs
@@ -1,67 +1,75 @@
 using System.Collections;
-using System.Collections.Generic;   
+using System.Collections.Generic;
 using UnityEngine;
-
 
 public class Movimentojogador : MonoBehaviour
 {
-    public float velocidadedojogador;
-    public float alturadopulo;
+    [Header("Movimentação")]
+    public float velocidadedojogador = 5f;
+    public float alturadopulo = 7f;
 
-
+    [Header("Componentes")]
     public Rigidbody2D origidbody2D;
     public SpriteRenderer ospriterenderer;
-    public bool estanochao;
+
+    [Header("Verificação de Chão")]
     public Transform verificadorDeChao;
-    public float raiodeverificacao;
+    public float raiodeverificacao = 0.2f;
     public LayerMask layerchao;
 
+    private bool estanochao;
+    private float inputHorizontal;
+    private bool desejaPular;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+        // Captura do input de movimento
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
 
-    private void FixedUpdate()
-    {
-     
-        movimentojogador();
-        pular();
-    }
-
-    public void movimentojogador()
-    {
-        float inputdomovimento = Input.GetAxisRaw("Horizontal");
-        origidbody2D.linearVelocity = new Vector2(inputdomovimento * velocidadedojogador, origidbody2D.linearVelocity.y);
-
-        if(inputdomovimento > 0)
+        // Captura do input de pulo usando tecla W
+        if (Input.GetKeyDown(KeyCode.W))
         {
+            desejaPular = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        VerificarChao();
+        MovimentarJogador();
+        Pular();
+        desejaPular = false; // Reset da flag após aplicar
+    }
+
+    void MovimentarJogador()
+    {
+        origidbody2D.linearVelocity = new Vector2(inputHorizontal * velocidadedojogador, origidbody2D.linearVelocity.y);
+
+        if (inputHorizontal > 0)
             ospriterenderer.flipX = false;
-        }
-        if(inputdomovimento < 0)
-        {
+        else if (inputHorizontal < 0)
             ospriterenderer.flipX = true;
-        }
-        
-
     }
 
-   
-    public void pular()
+    void VerificarChao()
     {
         estanochao = Physics2D.OverlapCircle(verificadorDeChao.position, raiodeverificacao, layerchao);
+    }
 
-        if (Input.GetKeyDown(KeyCode.W) && estanochao == true)
+    void Pular()
+    {
+        if (desejaPular && estanochao)
         {
-            origidbody2D.linearVelocity  = Vector2.up * alturadopulo;
+            origidbody2D.linearVelocity = Vector2.up * alturadopulo;
         }
+    }
 
+    void OnDrawGizmosSelected()
+    {
+        if (verificadorDeChao != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(verificadorDeChao.position, raiodeverificacao);
+        }
     }
 }
