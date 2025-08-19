6aa3b4b21ce14b733f8c6e633b6704dd855c7e13
LucasPagnoncelli <lucaspagnoncelli9@gmail.com>
2025-08-19 11:28:15 -0300

    ajuyda

--- Hollow/Assets/Scripts/Inimigo.cs
+++ Hollow/Assets/Scripts/Inimigo.cs
@@ -4,11 +4,50 @@ public class Inimigo : MonoBehaviour
 {
     public int vidaAtual;
     public GameObject Porta;
-    
+
+
+    public float velocidade = 2f;
+    private bool faceFlip = true; // controle de virada
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Mover();
+    }
+
+    private void Mover()
+    {
+        float direcao = faceFlip ? 1 : -1;
+        rb.velocity = new Vector2(direcao * velocidade, rb.velocity.y);
+    }
+
+    private void flipEnemy()
+    {
+        if (faceFlip)
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        else
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.collider.CompareTag("Player") || !collision.collider.CompareTag("Chao"))
+        {
+            faceFlip = !faceFlip; // agora funciona, é bool
+            flipEnemy();
+        }
+    }
+
+
     public void DanoNoInimigo(int dano)
     {
         vidaAtual -= dano;
-        if(vidaAtual <= 0)
+        if (vidaAtual <= 0)
         {
             Destroy(this.gameObject);
             Destroy(Porta);
--- Hollow/Assets/Scripts/triggerDamage.cs
+++ Hollow/Assets/Scripts/triggerDamage.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class triggerDamage : MonoBehaviour
 {
     public heartSystem heartSystem;
-    private void OnTriggerEnter2D(Collider2D collision)
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             heartSystem.vidaAtual--;
             heartSystem.dano();
-
         }
     }
 }
